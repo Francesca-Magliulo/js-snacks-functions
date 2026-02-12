e043c3927ab776981bc130dcaf1b1918a47264a4
Francesca-Magliulo <github.mf@gmail.com>
2026-02-12 16:27:05 +0100

    ex n 3 e 4 di ex-class completati

--- snack-funzioni/ex-class/js/script.js
+++ snack-funzioni/ex-class/js/script.js
@@ -6,8 +6,8 @@ const num = [2, 63, 9, 14, 56, 6, 12]
 function bigNum(num) {
     let max = num[0]
 
-    for(let i = 0; i < num.length; i++) {
-        if(num[i] > max) {
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] > max) {
             max = num[i]
         }
     }
@@ -24,8 +24,8 @@ const numArray = [63, 9, 14, 56, 6, 12, 1, 32, -1, -9, 5]
 function smallNum(numArray) {
     let min = numArray[0]
 
-    for(let i = 0; i < numArray.length; i++) {
-        if(numArray[i] < min) {
+    for (let i = 0; i < numArray.length; i++) {
+        if (numArray[i] < min) {
             min = numArray[i]
         }
     }
@@ -39,8 +39,36 @@ console.log(result1)
 //3) Creare una funzione che accetti come parametro un array e un numero e ritorna la posizione del numero nell'array o, se non c'è, ritorna -1
 
 
+const array = [32, 8, 5, 65, 4, 12, 9, -6, 28]
+
+function trovaNum(array, num) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === num) {
+            return i;
+        }
+    }
+    return -1
+}
+
+const result2 = trovaNum(array, 28)
+console.log(result2)
+
 
 //4) Creare una funzione che accetti come parametro un array e ritorna la somma degli elementi dell'array
+
+function sommaArray(array) {
+
+    const somma = array.reduce(function (acc, numero) {
+        return acc + numero;
+    }, 0);
+
+    return somma;
+
+}
+
+const resultSomma = sommaArray(array)
+console.log(resultSomma)
+
 
 //5) Creare una funzione che inverta una stringa passata come argomento
 
